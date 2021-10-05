6af34a9966437dae13cbaefc3741773f3954ab4a
tiendvlp <tiendvlp@gmail.com>
2021-10-05 19:07:56 +0700

    update test

--- src/test/java/login/LoginWithGoogleUseCaseTestSolution.java
+++ src/test/java/login/LoginWithGoogleUseCaseTestSolution.java
@@ -123,4 +123,147 @@ public class LoginWithGoogleUseCaseTestSolution {
     }
     //endregion
 
+    //region unit test
+
+    //region 1.ACCESS TOKEN IS PASSED TO GOOGLE ENDPOINT
+    @Test
+    public void loginWithGoogle_accessTokenPassedToEndPoint () {
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(googleGetUserEndpointTd.accessToken, ACCESS_TOKEN);
+    }
+    //endregion
+
+    //region 2.GOOGLE POJO IS PASSED CORRECTLY TO REPOSITORY
+    @Test
+    public void loginWithGoogle_googlePojoIsPassedCorrectlyToRepository () {
+        userRepository.isUserNotfound = true; // chưa đăng nhập vào ứng dụng
+        emailValidator.isFptEmail = true; // là thành viên của fpt
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(userRepository.user.getFullName(), GOOGLE_POJO.getFullName());
+        assertEquals(userRepository.user.getEmail(), GOOGLE_POJO.getEmail());
+    }
+    //endregion
+
+    //region 3.IF ACCESS TOKEN IS CORRECT THEN SUCCESS RESULT RETURNED
+    @Test ()
+    public void loginWithGoogle_correctAccessToken_successReturned () {
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof Success);
+    }
+    //endregion
+
+    //region 4.ACCESS TOKEN NOT FPT AND NOT IN DB, THEN NOT ALLOWED RETURNED
+    @Test
+    public void loginWithGoogle_accessTokenNotFptAndNotInDb_notAllowReturned () {
+        userRepository.isUserNotfound = true;
+        emailValidator.isFptEmail = false;
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof NotAllowed);
+    }
+    //endregion
+
+    //region 5.ACCESS TOKEN NOT FPT BUT IN DB, THEN SUCCESS RETURNED
+    @Test
+    public void loginWithGoogle_accessTokenNotFptButAdmin_successReturned () {
+        emailValidator.isFptEmail = false; // không phải email của fpt
+        userRepository.isUserNotfound = false; // nhưng có tồn tại trong database
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof Success);
+    }
+    //endregion
+
+    //region 6.ACCESS TOKEN IS NOT FPT AND NOT IN DB, THEN THERE IS NO INTERACTION WITH USER REPOSITORY ADD METHOD
+    @Test
+    public void loginWithGoogle_accessTokenIsNotFptAndNotInDb_noInteractionWithUserRepositoryAddMethod () {
+        emailValidator.isFptEmail = false;
+        userRepository.isUserNotfound = false;
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(userRepository.user, NON_INITIALIZE_USER);
+    }
+    //endregion
+
+    //region 7.ACCESS TOKEN IS FPT BUT NOT IN DB, THEN HAS INTERACTION WITH USER REPOSITORY ADD METHOD
+    @Test(groups = {HAS_INTERACTION_WITH_USER_REPOSITORY})
+    public void loginWithGoogle_accessTokenIsFptButNotInDb_hasInteractionWithUserRepositoryAddMethod () {
+        emailValidator.isFptEmail = true;
+        userRepository.isUserNotfound = true;
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(userRepository.user, USER);
+    }
+    //endregion
+
+    //region 8.ENDPOINT RETURN GENERAL ERROR THEN GENERAL ERROR RETURNED
+    @Test
+    public void loginWithGoogle_endPointGeneralError_generalErrorReturned () {
+        googleGetUserEndpointTd.isGeneralError = true;
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof GeneralError);
+    }
+    //endregion
+
+    //region 9.ENDPOINT RETURN AUTH ERROR THEN AUTH ERROR RETURNED
+    @Test
+    public void loginWithGoogle_endPointAuthError_authErrorReturned () {
+        googleGetUserEndpointTd.isAuthError = true;
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof AuthError);
+    }
+    //endregion
+
+    //region 10.ENDPOINT RETURN GENERAL ERROR, THEN THERE IS NO INTERACTION WITH REPOSITORY
+    @Test
+    public void loginWithGoogle_endPointGeneralError_noInteractionWithRepository () {
+        googleGetUserEndpointTd.isGeneralError = true;
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(userRepository.user, NON_INITIALIZE_USER);
+    }
+    //endregion
+
+    //region 11.ENDPOINT RETURN AUTHENTICATION ERROR, THEN THERE IS NO INTERACTION WITH REPOSITORY
+    @Test
+    public void loginWithGoogle_endPointAuthError_noInteractionWithRepository () {
+        googleGetUserEndpointTd.isAuthError = true;
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(userRepository.user, NON_INITIALIZE_USER);
+    }
+    //endregion
+
+    //region 12.ENDPOINT THROW CONNECTION EXCEPTION, THEN THERE IS NO INTERACTION WITH REPOSITORY
+    @Test
+    public void loginWithGoogle_endPointConnectionExceptionOccurs_noInteractionWithRepository () {
+        googleGetUserEndpointTd.isConnectionError = true;
+        SUT.executes(ACCESS_TOKEN);
+        assertEquals(userRepository.user, NON_INITIALIZE_USER);
+    }
+    //endregion
+
+    //region 13.ENDPOINT THROWS CONNECTION EXCEPTION THEN GENERAL ERROR RETURNED
+    @Test
+    public void loginWithGoogle_endPointConnectionExceptionOccurs_generalErrorReturned () {
+        googleGetUserEndpointTd.isConnectionError = true;
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof GeneralError);
+    }
+    //endregion
+
+    //region 14.USER REPOSITORY CONNECTION EXCEPTION OCCURS THEN GENERAL ERROR RETURNED
+    @Test
+    public void loginWithGoogle_userRepositoryConnectionExceptionOccurs_generalErrorReturned () {
+        userRepository.isConnectionExceptionOccurs = true;
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof GeneralError);
+    }
+    //endregion
+
+    //region 15.SUCCESS RESULT RETURN THE SAME USER THAT GET FROM REPOSITORY
+    @Test
+    public void loginWithGoogle_successReturned_successUserMatchWithRepositoryUserData () {
+        Result result = SUT.executes(ACCESS_TOKEN);
+        assertTrue(result instanceof Success);
+        assertEquals(((Success)result).user, USER);
+    }
+    //endregion
+
+    //endregion
+
 }
